184552e2b7da337c29dbc82f191f0070d8b06873
Luan Lisboa <luanslisboa@gmail.com>
2020-09-03 09:08:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,82 @@
-import nltk
-from flask import Flask
+from flask import Flask, make_response,request
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk import ngrams
 from nltk.tokenize import word_tokenize
+from string import punctuation
 
 app = Flask(__name__)
 
-@app.route('/Buscardoc/<text>')
+stop = stopwords.words('portuguese')
+ltext = []
+vec = {}
 
-#stop = stopwords.words('portuguese')
+@app.route('/')
+def index():
+    return "Hello! Send text to: 'site address'/request" \
+           "Get vector from: /response/<ngram>" \
+           "ngram can be null, 1 or 2"
 
-def GetDoc(text):
-    #stringbusca = [word for word in stringbusca if word not in stopwords.words('portuguese')]
+@app.route('/test')
+def testPost():
+    return '<form action="/request/" method="post">' \
+           '<input type="text" value="Falar é fácil. Mostre-me o código.">'\
+           '<input type="submit" value="text">' \
+           '</form>'
 
-    stop = stopwords.words('portuguese')
-    #stop.append('oito'.upper())
+# request
+@app.route('/request/', methods=['POST'])
+def request():
+    text = request.form['text']
+    ltext.append(text)
 
-    textTratado = text
+    return make_response("Text successfully added", 201)
 
-    for p in stop:
-        textTratado = textTratado.replace(' '+p+' ', ' ')
+# response
+@app.route('/response/<ngram>', methods=['GET'])
+def response(ngram):
+    for text in ltext:
+        tokenize(text,ngram)
+        vector(text)
 
-    tokens = nltk.tokenize.word_tokenize(textTratado)
-    fdist = FreqDist(tokens)
+    return vec
 
-    return fdist# str('trat: '+ textTratado + ' orig: ' + text)
+# cleaning
+def cleaning(text):
+    text = text.lower()
+    ilist = []
+    for p in punctuation:
+        text = text.replace(p,' ')
+    ilist = []
+    for w in text.split():
+        if w not in stop:
+            ilist.append(w)
 
-@app.route('/ngram/<text>/<ngram>')
+    text = " ".join(ilist)
+    return text
 
-def ngram(text,ngram):
+# generate token
+def tokenize(text,ngram):
+    n = 1
+    if ngram == None:
+        n = 1
+    else:
+        n = ngram
 
-    print(text)
-    print(ngram)
-    #stringbusca = [word for word in stringbusca if word not in stopwords.words('portuguese')]
+    for word in get_ngrams(text, n):
+        try:
+            vec[word] = 0
+        except:
+            pass
 
-    stop = stopwords.words('portuguese')
-    #stop.append('oito'.upper())
-
-    textTratado = text
-
-    for p in stop:
-        textTratado = textTratado.replace(' '+p+' ', ' ')
-
-    #tokens = nltk.tokenize.word_tokenize(textTratado)
-    #fdist = FreqDist(tokens)
-
-    print(textTratado)
-
-    #n_grams = ngrams(word_tokenize(textTratado), ngram)
-
-    r = get_ngrams(textTratado,int(ngram))
-
-    #print(n_grams)
-
-    print(r[0])
-
-    return str(r)
+def vector(text):
+    for word in vec:
+        vec[word] += text.count(word)
 
 def get_ngrams(text, n ):
     print(n)
     n_grams = ngrams(word_tokenize(text), n)
-    return [ ' '.join(grams) for grams in n_grams]
+    return [' '.join(grams) for grams in n_grams]
 
 if __name__ == '__main__':
     app.run()
